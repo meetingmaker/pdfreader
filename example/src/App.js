8865d6a4da0902a6804842f0b3a724b3646fb69d
Long Nguyen <nclong87@gmail.com>
2021-01-26 11:57:39 +0700

    Add presentation mode

--- example/src/App.js
+++ example/src/App.js
@@ -2,28 +2,56 @@ import React, { PureComponent } from 'react'
 
 import PDF from '@meetingmaker/pdfreader'
 
+const GoToPage = (props) => {
+  const inputRef= React.useRef();
+  function onClick() {
+    // console.log('onClick', Number(inputRef.current.value));
+    props.goToPage(Number(inputRef.current.value))
+  }
+  return (
+    <fieldset className="gotopage">
+      <legend>Go to page</legend>
+      <input defaultValue={1} type="text" ref={inputRef} />
+      <button onClick={onClick}>Go</button>
+    </fieldset>
+  )
+}
+
 export default class App extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
       open: true,
       downloadable: false,
+      isPresentationMode: false,
     }
     console.log('constructor')
     this.viewer = {
       host: 'http://localhost',
       path: '/pdf/web/viewer.html',
     };
+    this.pdfRef = React.createRef();
+    this.handleGoToPage = this.handleGoToPage.bind(this);
+    this.handleRequestPresentationMode = this.handleRequestPresentationMode.bind(this);
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ downloadable: true });
-    }, 5000);
-  }
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ downloadable: true });
+  //   }, 5000);
+  // }
 
   componentWillReceiveProps() {
     console.log('componentWillReceiveProps')
+  }
+
+  handleGoToPage(page) {
+    this.pdfRef.current.goToPage(page);
+  }
+
+  handleRequestPresentationMode() {
+    const isPresentationMode = !this.state.isPresentationMode;
+    this.setState({ isPresentationMode }, () => this.pdfRef.current.requestPresentationMode({ isPresentationMode }))
   }
 
   render () {
@@ -33,10 +61,17 @@ export default class App extends PureComponent {
       );
     }
     return (
-      <div>
+      <div className="pdf-container">
+        <div className="test-controls">
+          <GoToPage goToPage={this.handleGoToPage} />
+          <fieldset>
+            <legend>Request presentation mode</legend>
+            <button onClick={this.handleRequestPresentationMode}>Request</button>
+          </fieldset>
+        </div>
         <PDF
-          popup
-          src="https://awesomecloudstorage.blob.core.windows.net/aep/intrum/b87aa84f-6823-4b3f-ae5e-48b33ad66f97_fae9a42f-e653-47a8-b6e7-3c87cba8044a_https3.pdf"
+          ref={this.pdfRef}
+          src="https://file-examples-com.github.io/uploads/2017/10/file-example_PDF_500_kB.pdf"
           downloadable={this.state.downloadable}
           onRequestClose={() => this.setState({ open: false })}
           onLastPage={() => console.log('onLastPage')}
